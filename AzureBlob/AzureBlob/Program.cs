be4b889fea4708e49ee56f5732ab82c06d7b92d7
zekir <zekiriabd@hotmail.com>
2023-02-05 21:38:35 +0100

    Azure services

---
+++ AzureBlob/AzureBlob/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Storage.Blobs;
+
+// ---------- Const --------------------
+const string connectionString = "DefaultEndpointsProtocol=https;AccountName=nannytrackerstorage;AccountKey=Xdf+dvGsSg4lydVweYZ/+zuByaMtH0vwOqPgPBOfXsq+O2bRePDO817hyrdP4t8tKoVDTM+6w3k1+AStDLVO0Q==;EndpointSuffix=core.windows.net";
+const string containerName = "test";
+const string localFilePath = @"c:\test\zekiri.jpg";
+
+// ---------- Connection --------------------
+var container = new BlobContainerClient(connectionString, containerName);
+
+// -----------download--------------
+var blobfile = container.GetBlobClient("zekiri.jpg");
+await blobfile.DownloadToAsync(localFilePath);
+
+// ---------- UploadFile --------------------
+/*using (FileStream stream = File.OpenRead(localFilePath))
+{
+    string filename = Path.GetFileName(localFilePath);
+    await container.UploadBlobAsync(filename, stream);
+}*/
+
---
+++ AzureFiles/AzureFiles/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure;
+using Azure.Storage.Files.Shares;
+
+// ---------- Const --------------------
+const string connectionString = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const string storageAccount = "logs";
+const string localFilePath = @"c:\test\app.log";
+
+// ---------- Connection --------------------
+ShareClient cloudFileShare = new(connectionString, storageAccount);
+ShareDirectoryClient directory = cloudFileShare.GetDirectoryClient("dir1");
+
+// ---------- UploadFile --------------------
+ShareFileClient file = directory.GetFileClient("New.log");
+using (FileStream stream = File.OpenRead(localFilePath))
+{
+    file.Create(stream.Length);
+    await file.UploadRangeAsync(new HttpRange(0, stream.Length), stream);
+}
+
+// -----------Delete--------------
+//await file.DeleteIfExistsAsync();
+
+// -----------download--------------
+//ShareFileDownloadInfo download = await file.DownloadAsync();
+//using (FileStream stream = File.OpenWrite(localFilePath))
+//{
+//    await download.Content.CopyToAsync(stream);
+//    await stream.FlushAsync();
+//    stream.Close();
+//}
